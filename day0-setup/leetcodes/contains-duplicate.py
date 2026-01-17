d6bdf4f8eb4a21028e4bfbbc51694af2fe329750
Ajmir Aribam <arajmir7@gmail.com>
2026-01-17 19:57:48 +0530

    leetcode: contains duplicate

---
+++ day0-setup/leetcodes/contains-duplicate.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        seen = set()
+
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+
+        return False
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.containsDuplicate([1,2,3,1]))  # True
+    print(sol.containsDuplicate([1,2,3,4]))  # False
